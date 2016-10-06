58f412ac559193c88fdcebf35df8451aabff9020
AlexandrPetkov <petkov.alexandr@gmail.com>
2016-10-06 16:47:23 +0300

    1-я задача сделана

---
+++ src/topic5/task1/Fraction.java
@@ -0,0 +1,25 @@
+package topic5.task1;
+
+import java.util.List;
+
+public class Fraction {
+    void printFr(ПростаяДробь fr){
+        if (fr != null)
+        System.out.println(fr);
+    }
+    
+    void printFrArray(ПростаяДробь[] fr){
+        if (fr != null)
+        for (int i = 0; i < fr.length; i++) {
+            printFr(fr[i]);
+        }
+    }
+    
+    void printFrCollection(List<ПростаяДробь> fr){
+        if (fr != null)
+        for (ПростаяДробь iterator:
+             fr) {
+            printFr(iterator);
+        }
+    }
+}
---
+++ src/topic5/task1/ПростаяДробь.java
@@ -0,0 +1,71 @@
+package topic5.task1;
+
+public class ПростаяДробь {
+    int numerator;
+    int denominator;
+
+    public ПростаяДробь(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+        if (numerator < 0 && denominator < 0){
+            this.numerator=-numerator;
+            this.denominator=-denominator;
+        }
+    }
+    void summ(ПростаяДробь fraction){
+        if (fraction != null || notZero(fraction))
+        {
+            numerator+=fraction.numerator;
+            denominator+=fraction.denominator;
+        }
+    }
+
+    void mult(ПростаяДробь fraction){
+        if (notZero(fraction) && notZero(this)){
+            numerator*=fraction.numerator;
+            denominator*=fraction.denominator;
+        }
+    }
+
+    void minus(ПростаяДробь fraction){
+        if (fraction != null || notZero(fraction))
+        {
+            numerator-=fraction.numerator;
+            denominator-=fraction.denominator;
+        }
+
+    }
+
+    void division(ПростаяДробь fraction){
+        if (notZero(fraction) && notZero(this)){
+            numerator*=fraction.denominator;
+            denominator*=fraction.numerator;
+        }
+
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    private boolean notZero (ПростаяДробь fr){
+        return fr.numerator!=0 || fr.denominator!=0;
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/"+denominator;
+    }
+}
